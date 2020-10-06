40ec759f27755d88aba93e6846bbd6f29cf263d6
anju <anju.p@momscode.in>
2020-10-06 16:44:45 +0530

    added duplication

--- ticket_management/ticket_management/doctype/ticket/ticket.js
+++ ticket_management/ticket_management/doctype/ticket/ticket.js
@@ -178,7 +178,7 @@ frappe.ui.form.on('Ticket', {
 			var to_date = Date.parse(d.expected_end_date);
 
 			if(to_date < from_date){
-				frappe.msgprint(__("Expected End Date Should be After Expected Start Date"));
+				frappe.msgprint(__("Planned End Date Should be After Planned Start Date"));
 				frm.set_value('expected_end_date', '');
 				return;
 			}
@@ -193,12 +193,139 @@ frappe.ui.form.on('Ticket', {
 			var to_date = Date.parse(d.completion_date);
 
 			if(to_date < from_date){
-				frappe.msgprint(__("Completed Date Should be After Expected Start Date"));
+				frappe.msgprint(__("Actual Date Should be After Planned Start Date"));
 				frm.set_value('completion_date', '');
 				return;
 			}
 		}
-	}
-	
+	},
+	validate:function(frm,cdt,cdn){	
+		var d = locals[cdt][cdn];
+	$.each(frm.doc.details || [], function(i, v) {
+		var from_date = Date.parse(d.expected_start_date);
+			var to_date = Date.parse(v.date);
+			var date = Date.parse(d.completion_date);
+		if((from_date >to_date)||(date<to_date)){
+			frappe.msgprint(__("Start Date Should be Within the Ticket Planned Start Date and Planned End Date"));
+			validated = false;
+		}
+	});
+}
+});
+frappe.ui.form.on('Ticket Details', {
+	status:function(frm,cdt,cdn){
+		var d = locals[cdt][cdn];
+	$.each(frm.doc.details || [], function(i, v) {
+		var from_date = Date.parse(d.expected_start_date);
+			var to_date = Date.parse(v.date);
+			var date = Date.parse(d.completion_date);
+		if((from_date >to_date)||(date<to_date)){
+			frappe.msgprint(__("Start Date Should be Within the Ticket Planned Start Date and Planned End Date"));
+			validated = false;
+		}
+	})
+	var count=0
+	var count1=0
+	var entry_datetime=d.start_time
+	var res = entry_datetime.split(":");
+	var displayVal = res[0]+ ':' + res[1]
+	var exit_datetime=d.end_time
+	var end = exit_datetime.split(":");
+	var End_val = end[0]+ ':' + end[1]
+	$.each(frm.doc.details || [], function(i, v) {
+		var entry_datetime=v.start_time
+		var res1 = entry_datetime.split(":");
+		var displayVal1 = res1[0]+ ':' + res1[1]
+		var exit_datetime1=v.end_time
+		var end1 = exit_datetime1.split(":");
+		var End_val1 = end1[0]+ ':' + end1[1]
+		var a=0;
+		
+		if((d.date==v.date)&&(displayVal==displayVal1)&&(End_val1==End_val))
+		{
+			count1++;
+		}
+		if((d.type_of_task==v.type_of_task)&&(d.date==v.date)&&(displayVal==displayVal1)&&(End_val1==End_val))
+		{
+			count++;
+		}
+		
 
+	})
+	if(count1>1)
+	{
+		frappe.msgprint(__("Can't Enter Different Activity on Same Duration"));
+		validated = false;
+		frappe.model.set_value(d.doctype, d.name,"type_of_task",'')
+		frappe.model.set_value(d.doctype, d.name,"date",'')
+		frappe.model.set_value(d.doctype, d.name,"start_time",'')
+		frappe.model.set_value(d.doctype, d.name,"end_time",'')
+		frappe.model.set_value(d.doctype, d.name,"status",'')
+        //frm.doc.details.splice(frm.doc.details[details_idx], 1)
+		//frm.refresh_field('details')
+		
+	}
+	if(count>1)
+	{
+		frappe.msgprint(__("Activity alredy Entered"));
+		validated = false;
+		frappe.model.set_value(d.doctype, d.name,"type_of_task",'')
+		frappe.model.set_value(d.doctype, d.name,"date",'')
+		frappe.model.set_value(d.doctype, d.name,"start_time",'')
+		frappe.model.set_value(d.doctype, d.name,"end_time",'')
+		frappe.model.set_value(d.doctype, d.name,"status",'')
+        //frm.doc.details.splice(frm.doc.details[details_idx], 1)
+		//frm.refresh_field('details')
+		
+	}
+
+	
+},
+
+type_of_task:function(frm,cdt,cdn){
+	var d = locals[cdt][cdn];
+$.each(frm.doc.details || [], function(i, v) {
+	var from_date = Date.parse(d.expected_start_date);
+		var to_date = Date.parse(v.date);
+		var date = Date.parse(d.completion_date);
+	if((from_date >to_date)||(date<to_date)){
+		frappe.msgprint(__("Start Date Should be Within the Ticket Planned Start Date and Planned End Date"));
+		validated = false;
+	}
+})
+var count=0
+var entry_datetime=d.start_time
+var res = entry_datetime.split(":");
+var displayVal = res[0]+ ':' + res[1]
+var exit_datetime=d.end_time
+var end = exit_datetime.split(":");
+var End_val = end[0]+ ':' + end[1]
+$.each(frm.doc.details || [], function(i, v) {
+	var entry_datetime=v.start_time
+	var res1 = entry_datetime.split(":");
+	var displayVal1 = res1[0]+ ':' + res1[1]
+	var exit_datetime1=v.end_time
+	var end1 = exit_datetime1.split(":");
+	var End_val1 = end1[0]+ ':' + end1[1]
+	var a=0;
+	if((d.date==v.date)&&(displayVal==displayVal1)&&(End_val1==End_val))
+	{
+		count++;
+	}
+
+})
+if(count>1)
+{
+	frappe.msgprint(__("Cant Enter Two activities in same duration"));
+	validated = false;
+	frappe.model.set_value(d.doctype, d.name,"type_of_task",'')
+	frappe.model.set_value(d.doctype, d.name,"date",'')
+	frappe.model.set_value(d.doctype, d.name,"start_time",'')
+	frappe.model.set_value(d.doctype, d.name,"end_time",'')
+	frappe.model.set_value(d.doctype, d.name,"status",'')
+	//frm.doc.details.splice(frm.doc.details[details_idx], 1)
+	//frm.refresh_field('details')
+	
+}
+}
 });
